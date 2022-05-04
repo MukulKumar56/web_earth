8bfe3cc11054110a8fec95f431bb2103cabfb08b
Mukul Kumar <101646861+MukulKumar56@users.noreply.github.com>
2022-05-04 15:48:49 +0530

    updated navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -14,14 +14,14 @@ const Navbar = () => {
 
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
-          <Link className="navbar-brand" to="/">Web Earth</Link>
+          <Link className="navbar-brand"  to="/web_earth">Web Earth</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className='nav-link' aria-current="page" to="/">Home</Link>
+                <Link className='nav-link' aria-current="page" to="/web_earth">Home</Link>
               </li>
               <li className="nav-item">
                 <Link className='nav-link' to="/post2">Tips</Link>
